fd3b52ef757201a7f4ff1cc955905c99fab39f68
Crowdbotics <team@crowdbotics.com>
2023-11-09 14:17:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-meadow-44445",
+  url: "https://rapid-meadow-44445.botics.co",
+  api: "https://rapid-meadow-44445.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
